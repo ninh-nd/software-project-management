3bac6b84d250bdbf1e95d2ba5c6738939d42ffb9
ninh-nd <nguyendangninh0@gmail.com>
2022-11-05 21:01:48 +0700

    Temporarily use online backend only

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export default axios.create({
-    baseURL: process.env.API_ENDPOINT || 'http://localhost:3001/v1/',
+    baseURL: 'https://dashboard-api.up.railway.app/v1/',
     headers: {
         'Content-Type': 'application/json'
     }
