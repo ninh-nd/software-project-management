a145fe89bcc86eebb21ce90e59f722cfb8fc690e
ninh-nd <nguyendangninh0@gmail.com>
2022-12-12 10:33:48 +0700

    Center error component

--- src/components/common/ErrorLoadingComponent.tsx
+++ src/components/common/ErrorLoadingComponent.tsx
@@ -2,7 +2,7 @@ import { Box, Typography } from "@mui/material";
 
 export default function ErrorLoadingComponent() {
     return (
-        <Box>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexGrow: '1' }}>
             <Typography variant="h4">Error loading component</Typography>
         </Box>
     )
