1a7ddaad6abc3e764950df1bfa459d515bb5b90b
ninh-nd <nguyendangninh0@gmail.com>
2022-12-13 17:19:28 +0700

    Add logout call

--- src/actions/accountAction.ts
+++ src/actions/accountAction.ts
@@ -3,6 +3,10 @@ export async function login(username: string, password: string) {
     const response = await api.post('/account/login', { username, password });
     return response;
 }
+export async function logout() {
+    const response = await api.post('/account/logout');
+    return response;
+}
 export async function getAccountRole(username: string) {
     const response = await api.get(`/account/role?username=${username}`);
     return response.data;
