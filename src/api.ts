8b2593b5fb2b457c456c265665858a85a34d6d4d
ninh-nd <nguyendangninh0@gmail.com>
2023-01-07 21:36:59 +0700

    Add API base URL to deploy

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,7 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import axios from 'axios'
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+const API_BASE_URL = process.env.API_BASE_URL
 export default axios.create({
     baseURL: API_BASE_URL,
     headers: {
