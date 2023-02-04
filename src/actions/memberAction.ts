dd99ab7feb4ae315d6f51a596414b5b4e4ded5c0
ninh-nd <nguyendangninh0@gmail.com>
2023-02-04 22:03:32 +0700

    Rewrite mark task

--- src/actions/memberAction.ts
+++ src/actions/memberAction.ts
@@ -7,11 +7,12 @@ export async function getMembersOfProject(
   const response = await api.get(`/project/${projectName}/member`);
   return response.data;
 }
-/* Need to be rewrite someday */
 export async function markTask(taskIdArray: string[], status: string) {
-  taskIdArray.forEach(async (taskId) => {
-    await api.put(`/task`, { status });
+  const response = await api.patch("/task", {
+    data: taskIdArray,
+    status: status,
   });
+  return response.data;
 }
 export async function assignTask(
   taskId: string,
