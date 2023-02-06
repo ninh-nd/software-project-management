56c2e1b3ef1cbe80d181371b1efa3f769ae4f22c
ninh-nd <nguyendangninh0@gmail.com>
2023-02-06 20:01:26 +0700

    Add route to ticket page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,13 +2,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SnackbarProvider } from "notistack";
 import { Routes, Route } from "react-router-dom";
 import DashboardLayout from "~/layouts/DashboardLayout";
-import AccountInfo from "./account/AccountInfo";
-import Home from "./home/Home";
-import Login from "./login/Login";
-import MemberInfo from "./member/MemberInfo";
-import PhaseInfo from "./phaseInfo/PhaseInfo";
-import TaskInfo from "./tasks/TaskInfo";
-import VulnerabilityPage from "./vulnerabilities/VulnerabilityPage";
+import AccountInfo from "./AccountInfo";
+import Home from "./Home";
+import TicketPage from "./TicketPage";
+import Login from "./Login";
+import MemberInfo from "./MemberInfo";
+import PhaseInfo from "./PhaseInfo";
+import TaskInfo from "./TaskInfo";
+import VulnerabilityPage from "./VulnerabilityPage";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -29,6 +30,7 @@ export default function App() {
             <Route path="" element={<Home />} />
             <Route path="phases" element={<PhaseInfo />} />
             <Route path="tasks" element={<TaskInfo />} />
+            <Route path="tickets" element={<TicketPage />} />
             <Route path="memberInfo" element={<MemberInfo />} />
           </Route>
           <Route path="/login" element={<Login />} />
