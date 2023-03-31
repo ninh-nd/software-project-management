97007e65b3cab796988c21337f9a495b69a9d4d2
ninh-nd <ninh.nd194815@sis.hust.edu.vn>
2023-03-31 11:14:22 +0700

    Merge chart

--- src/components/home/chart/Chart.tsx
+++ src/components/home/chart/Chart.tsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from "@mui/material";
 import {
   Bar,
   BarChart,
@@ -9,54 +10,48 @@ import {
   YAxis,
 } from "recharts";
 import Title from "~/components/common/Title";
+import { useThemeHook } from "~/hooks/theme";
 import { ICommits, IPullRequests } from "~/interfaces/GithubData";
 import InfoPaper from "../InfoPaper";
 type ChartProps = {
   commits: ICommits;
   prs: IPullRequests;
 };
+function mergeContributions(commits: ICommits, prs: IPullRequests) {
+  const merged = [];
+  const contributions = commits.contribution.concat(prs.contribution);
+  const authors = [...new Set(contributions.map((c) => c.author))];
+
+  for (const author of authors) {
+    const totalCommits =
+      commits.contribution.find((c) => c.author === author)?.total || 0;
+    const totalPrs =
+      prs.contribution.find((c) => c.author === author)?.total || 0;
+    merged.push({ author, totalCommits, totalPrs });
+  }
+
+  return merged;
+}
 export default function Chart({ commits, prs }: ChartProps) {
+  const theme = useThemeHook();
+  const lessThanMedium = useMediaQuery(theme.breakpoints.down("md"));
+  const merged = mergeContributions(commits, prs);
   return (
     <InfoPaper>
       <Title>Activity history</Title>
       <ResponsiveContainer width="100%" aspect={3 / 1}>
-        <BarChart
-          width={500}
-          height={300}
-          data={commits.contribution}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="author" />
+        <BarChart width={500} height={300} data={merged}>
+          <CartesianGrid strokeDasharray="3 3" />{" "}
+          <XAxis dataKey="author" hide={lessThanMedium} />
           <YAxis />
           <Tooltip />
           <Legend />
-          <Bar dataKey="total" fill="#8884d8" name="Number of commits" />
-        </BarChart>
-      </ResponsiveContainer>
-      <ResponsiveContainer width="100%" aspect={3 / 1}>
-        <BarChart
-          width={500}
-          height={300}
-          data={prs.contribution}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="author" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="total" fill="#82ca9d" name="Number of pull requests" />
+          <Bar dataKey="totalCommits" fill="#8884d8" name="Number of commits" />
+          <Bar
+            dataKey="totalPrs"
+            fill="#82ca9d"
+            name="Number of pull requests"
+          />
         </BarChart>
       </ResponsiveContainer>
     </InfoPaper>
