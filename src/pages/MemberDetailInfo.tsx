50cb31176f8f686380521dca4a04adfc2d099a13
ninh-nd <ninh.nd194815@sis.hust.edu.vn>
2023-03-31 11:33:30 +0700

    Responsive member page

--- src/pages/MemberDetailInfo.tsx
+++ src/pages/MemberDetailInfo.tsx
@@ -1,10 +1,9 @@
-import { Box, Grid, Stack } from "@mui/material";
+import { Grid, Stack } from "@mui/material";
 import { useParams } from "react-router-dom";
 import ActivityHistoryCard from "~/components/memberInfo/ActivityHistoryCard";
 import MemberInfoCard from "~/components/memberInfo/MemberInfoCard";
 import TaskCard from "~/components/memberInfo/TaskCard";
 import { useMemberQuery } from "~/hooks/query";
-import { IMember } from "~/interfaces/Member";
 
 export default function MemberDetailInfo() {
   const { memberId } = useParams();
@@ -13,18 +12,16 @@ export default function MemberDetailInfo() {
   const member = memberQuery.data?.data;
   if (!member) return <></>;
   return (
-    <Box flexGrow={1} height="100vh" sx={{ p: 10 }}>
-      <Grid container spacing={3}>
-        <Grid item xs={2}>
-          <MemberInfoCard member={member} />
-        </Grid>
-        <Grid item xs={10}>
-          <Stack spacing={3}>
-            <ActivityHistoryCard member={member} />
-            <TaskCard member={member} />
-          </Stack>
-        </Grid>
+    <Grid container spacing={3} sx={{ p: 2 }}>
+      <Grid item xs={12} md={2}>
+        <MemberInfoCard member={member} />
       </Grid>
-    </Box>
+      <Grid item xs={12} md={10}>
+        <Stack spacing={3}>
+          <ActivityHistoryCard member={member} />
+          <TaskCard member={member} />
+        </Stack>
+      </Grid>
+    </Grid>
   );
 }
