971e5462c3ba9f8dec67b9afdb1462d771ec1abb
Dang Ninh <ninh.nd194815@sis.hust.edu.vn>
2023-05-08 20:33:04 +0700

    Add a hook that checks for role to render

--- src/hooks/general.ts
+++ src/hooks/general.ts
@@ -5,3 +5,9 @@ export const usePermissionHook = () => {
   if (!accountInfo) return [];
   return accountInfo.permission;
 };
+export const useUserRole = () => {
+  const accountInfoQuery = useAccountInfoQuery();
+  const accountInfo = accountInfoQuery.data?.data;
+  if (!accountInfo) return [];
+  return accountInfo.role;
+};
