1c28fd73d96f426826be0e063fd917e21ce5edf5
Dang Ninh <ninh.nd194815@sis.hust.edu.vn>
2023-05-16 15:38:03 +0700

    Move threat info to a separate file

---
+++ src/components/common/vulnPage/forms/displayInfo.ts
@@ -0,0 +1,32 @@
+export const typeOptions = [
+  {
+    name: "Spoofing",
+    description:
+      "Threat action aimed at accessing and use of another user’s credentials, such as username and password.",
+  },
+  {
+    name: "Tampering",
+    description:
+      "Threat action intending to maliciously change or modify persistent data, such as records in a database, and the alteration of data in transit between two computers over an open network, such as the Internet.",
+  },
+  {
+    name: "Repuditation",
+    description:
+      "Threat action aimed at performing prohibited operations in a system that lacks the ability to trace the operations.",
+  },
+  {
+    name: "Information Disclosure",
+    description:
+      "Threat action intending to read a file that one was not granted access to, or to read data in transit.",
+  },
+  {
+    name: "Denial of Service",
+    description:
+      "Threat action attempting to deny access to valid users, such as by making a web server temporarily unavailable or unusable.",
+  },
+  {
+    name: "Elevation of Privilege",
+    description:
+      "Threat action intending to gain privileged access to resources in order to gain unauthorized access to information or to compromise a system.",
+  },
+];
